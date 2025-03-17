3083f860c0863a0bc2f7dae5c638de83059c0e8f
SUPRIYAKUKUNURU <supriyakukunuru2001@gmail.com>
2025-03-17 11:23:48 +0530

    first commit

--- src/Search.jsx
+++ src/Search.jsx
@@ -6,7 +6,7 @@ const Search = () => {
   
   return (
     <div className="container mt-3 d-flex justify-content-between align-items-center p-3" style={{ backgroundColor: "#EEEEEE" }}>
-      {/* Search Bar */}
+      
       <div className="input-group" style={{ maxWidth: "500px" }}>
         <input type="text" className="form-control border-0" placeholder="Search your products here" style={{ boxShadow: "none" }} />
         <span className="input-group-text bg-white border-0">
@@ -14,7 +14,7 @@ const Search = () => {
         </span>
       </div>
 
-      {/* User and Cart Icons */}
+      
       <div className="d-flex gap-4">
         <FontAwesomeIcon icon={faUser} size="lg" />
         <Link style={{color:"black"}} to="/cart">
